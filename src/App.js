3cf570554b6346bf2560e377bbc8d8448c098474
PabloPap <ppapad_33@yahoo.gr>
2018-10-02 11:26:26 +0300

    react tools-parcel-jsx-rendering data

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { render } from "react-dom";
+import pf from "petfinder-client";
+import Pet from "./Pet";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: []
+    };
+  }
+  componentDidMount() {
+    // const promise = petfinder.breed.list({ animal: "dog" });
+    // promise.then(console.log, console.error);
+    petfinder.pet
+      .find({ output: "full", location: "Seattle, WA" })
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+        this.setState({
+          pets
+        });
+      });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Adopt Me!</h1>
+        {/* dump all the state into the dom */}
+        {/* <pre>
+          <code>{JSON.stringify(this.state, null, 4)}</code>
+        </pre> */}
+        <div>
+          {this.state.pets.map(pet => {
+            let breed;
+            if (Array.isArray(pet.breeds.breed)) {
+              breed = pet.breeds.breed.join(", ");
+            } else {
+              breed = pet.breeds.breed;
+            }
+            return (
+              <Pet
+                key={pet.id}
+                animal={pet.animal}
+                name={pet.name}
+                breed={breed}
+                media={pet.media}
+                location={`${pet.contact.city}, ${pet.contact.state}`}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
---
+++ src/Pet.js
@@ -0,0 +1,43 @@
+import React from "react";
+// from createElement calls to jsx
+// const Pet = props => {
+//   // return React.createElement("div", {}, [
+//   //   React.createElement("h1", {}, props.name),
+//   //   React.createElement("h2", {}, props.animal),
+//   //   React.createElement("h2", {}, props.breed)
+//   // ]);
+
+//   return (
+//     <div>
+//       <h1>{props.name}</h1>
+//       <h2>{props.animal}</h2>
+//       <h2>{props.breed}</h2>
+//     </div>
+//   );
+// };
+class Pet extends React.Component {
+  render() {
+    const { name, animal, breed, media, location } = this.props;
+
+    let photos = [];
+
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
+    return (
+      <div className="pet">
+        <div className="image-container">
+          <img src={photos[0].value} alt={name} />
+        </div>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Pet;
